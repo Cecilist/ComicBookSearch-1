0f4ea61a7ecefb36a08c9bfc3c80085dd82d5ff2
Jacob Cecil <jcecil239@gmail.com>
2021-03-11 20:55:47 -0500

    Superhero now passes null on ioexception, superheroList is only built is super is not null

--- src/main/java/edu/bsu/cs222/Superhero.java
+++ src/main/java/edu/bsu/cs222/Superhero.java
@@ -8,6 +8,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+
 import java.util.List;
 
 public class Superhero {
@@ -55,7 +56,7 @@ public class Superhero {
             charDoesntExist.setTitle("Character Does Not Exist!");
             charDoesntExist.setContentText("The Character does not exist, please try again!");
             charDoesntExist.show();
-            System.exit(1);
+            return null;
         }
 
         return superheros;
