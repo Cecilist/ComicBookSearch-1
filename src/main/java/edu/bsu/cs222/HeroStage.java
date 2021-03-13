3d1eb54ddd9e95a29ca1353f7bca70ce5630f71b
Jacob Cecil <jcecil239@gmail.com>
2021-03-12 20:46:09 -0500

    Fixed the Corn issue: Created new variable in HeroStage, noComicsFlag, that will increment is a character from search results have no comics. If that variable = superheroList.size, then a pop appears telling user that the character exists but with no comics.

--- src/main/java/edu/bsu/cs222/HeroStage.java
+++ src/main/java/edu/bsu/cs222/HeroStage.java
@@ -1,8 +1,10 @@
 package edu.bsu.cs222;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -30,6 +32,7 @@ public class HeroStage extends Stage {
             ScrollPane buttonScroll = new ScrollPane(superheroButtons);
             superheroButtons.setSpacing(5);
             superheroButtons.setAlignment(Pos.CENTER);
+            int noComicsFlag = 0;
             for (int i = 0; i < superheroList.size(); i++) {
                 if(superheroList.get(i).hasComics()) {
                     Button superHeroButton = new Button(superheroList.get(i).getName());
@@ -37,11 +40,22 @@ public class HeroStage extends Stage {
                     superHeroButton.setOnMouseClicked(event -> comicStage.comicBooks(superheroList.get(finalI)));
                     superheroButtons.getChildren().add(superHeroButton);
                 }
+                else noComicsFlag++;
+
             }
-            superheroButtons.setBackground(new Background(
-                    new BackgroundFill(Color.web("#F0131E"), CornerRadii.EMPTY, Insets.EMPTY)));
-            setScene(new Scene(buttonScroll));
-            showAndWait();
+            if (noComicsFlag != superheroList.size()) {
+                superheroButtons.setBackground(new Background(
+                        new BackgroundFill(Color.web("#F0131E"), CornerRadii.EMPTY, Insets.EMPTY)));
+                setScene(new Scene(buttonScroll));
+                showAndWait();
+            }
+            else {
+                Alert charHasNoComics = new Alert(Alert.AlertType.ERROR);
+                charHasNoComics.setTitle("Character has no comics!");
+                charHasNoComics.setContentText("The Marvel Character, "+ superheroList.get(0).getName()+
+                        ", exists but is not featured in any comics, please try again!");
+                charHasNoComics.showAndWait();
+            }
         }
     }
 }
