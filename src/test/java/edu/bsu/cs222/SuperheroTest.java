c00eb2eb98a8d2f543330bf304b438249e5d0a58
Jacob Cecil <jcecil239@gmail.com>
2021-03-28 18:48:12 -0400

    Changed Test Method Names. Added a few tests for if we have null data. Changed ComicBook to pass null when no creators are found for chosen comicbook. Fixed issue where if a comicbook had an invalid date, no description window would open. Achieved by having getFormattedSaleDate return null if there is an invalid on sale date.

--- src/test/java/edu/bsu/cs222/SuperheroTest.java
+++ src/test/java/edu/bsu/cs222/SuperheroTest.java
@@ -24,19 +24,19 @@ public class SuperheroTest {
     }
 
     @Test
-    public void getNameTest() {
+    public void getName_SpiderMan_ReturnSpiderMan() {
         Assertions.assertEquals("Spider-Man", spiderMan.getName());
     }
 
     @Test
-    void getDescriptTest() {
+    void getDescription_SpiderMan_ReturnDescription() {
         Assertions.assertEquals("Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. " +
                 "Taught that with great power comes great responsibility, " +
                 "Spidey has vowed to use his powers to help people.", spiderMan.getDescription());
     }
 
     @Test
-    void getIdTest() {
+    void getId_SpiderMan_ReturnID() {
         Assertions.assertEquals("1009610", spiderMan.getId());
     }
 }
