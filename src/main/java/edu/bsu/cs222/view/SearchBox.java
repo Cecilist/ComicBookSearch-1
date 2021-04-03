86e5d8f69a2f269e935d11c1a65f5a3e745d3977
Jacob Cecil <Jcecil239@gmail.com>
2021-04-03 14:32:57 -0400

    Deleted commented out code

--- src/main/java/edu/bsu/cs222/view/SearchBox.java
+++ src/main/java/edu/bsu/cs222/view/SearchBox.java
@@ -100,7 +100,6 @@ public class SearchBox extends VBox {
             initialStage newInitialStage = new initialStage();
             newInitialStage.createStage( primaryStage);
         });
-        //backButton.setDisable(true);
         return backButton;
     }
 }
