abde7401e05271ee22bdca2dca9165de205d8782
Lloyd Rowe <lr00429@gmail.com>
2021-04-04 01:33:03 -0400

    Reduced Redundancy HeroBox

--- src/main/java/edu/bsu/cs222/view/initialStage.java
+++ src/main/java/edu/bsu/cs222/view/initialStage.java
@@ -63,7 +63,7 @@ public class initialStage extends VBox {
         Button superheroSearchButton = new Button("Superhero Search");
         superheroSearchButton.setOnAction(event -> {
             SearchBox createStage = new SearchBox();
-            createStage.createStage(primaryStage, "Character", true);
+            createStage.createStage(primaryStage, "character", true);
         });
         return superheroSearchButton;
     }
@@ -71,7 +71,7 @@ public class initialStage extends VBox {
         Button superheroSearchButton = new Button("Creator Search");
         superheroSearchButton.setOnAction(event -> {
             SearchBox createStage = new SearchBox();
-            createStage.createStage(primaryStage, "Creator", false);
+            createStage.createStage(primaryStage, "creator", false);
         });
         return superheroSearchButton;
     }
