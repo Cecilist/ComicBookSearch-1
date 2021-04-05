bd65edbd86cbb3ba88c315bd0705ebb375d1b7a0
Lloyd Rowe <lr00429@gmail.com>
2021-04-04 22:01:11 -0400

    Modified Creator Test

---
+++ src/test/java/edu/bsu/cs222/CreatorTest.java
@@ -0,0 +1,39 @@
+package edu.bsu.cs222;
+
+import com.jayway.jsonpath.JsonPath;
+import edu.bsu.cs222.model.Creator;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CreatorTest {
+    private final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("StanLee.json");
+    private Creator stanLee = new Creator();
+
+    {
+        try {
+            JSONArray charData = JsonPath.read(inputStream, "*");
+            stanLee = stanLee.buildCreator(charData).get(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getName_Stan_Lee_ReturnStan_Lee() {
+        Assertions.assertEquals("Stan Lee", stanLee.getName());
+    }
+
+    @Test
+    public void getComicsTotal_1662_ReturnStan_Lee() {
+        Assertions.assertEquals(1662, stanLee.getComicsTotal());
+    }
+
+    @Test
+    void getId_30_ReturnID() {
+        Assertions.assertEquals("30", stanLee.getId());
+    }
+}
